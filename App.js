ce7ab0e21ecf0ad8e93c28d7280ba28168a807e6
Antoine Goncalves <antoine.goncalves-isc.france@ibm.com>
2023-02-09 16:51:04 +0100

    Bien structurer son application

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {StyleSheet, Text} from 'react-native';
+
+const App = ({children, title}) => {
+  return <Text>Hello World !</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default App;

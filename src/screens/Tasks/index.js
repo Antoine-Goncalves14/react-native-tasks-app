27b565d5e9726697766982df33e4f0cea73a3f74
Antoine Goncalves <antoine.goncalves-isc.france@ibm.com>
2023-02-10 14:32:23 +0100

    Structure et logique de TasksScreen

--- src/screens/Tasks/index.js
+++ src/screens/Tasks/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import Header from '../../components/Header';
 
 export default function TasksScreen() {
+  // Liste de tâches
+  // State pour garder en mémoire les tâches
+
+  // 2x TasksCounter => props nb & title
+  // TasksList => return FlatList => TaskTile
+
   return (
     <>
       <Header />

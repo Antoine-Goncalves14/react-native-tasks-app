1b65a03d9d21998f31cdf7c905f2475e4c723f11
Antoine Goncalves <antoine.goncalves-isc.france@ibm.com>
2023-02-13 15:45:05 +0100

    création du reducer TasksList

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import {legacy_createStore as createStore} from 'redux';
+import {legacy_createStore as createStore, combineReducers} from 'redux';
 
 // Selectors: sélectionner une partie de notre store
 
@@ -6,5 +6,21 @@ import {legacy_createStore as createStore} from 'redux';
 
 // Reducers: fonctions retournent un nouveau state
 
+const initialState = [{id: 1, title: 'Init task', isCompleted: false}];
+
+const tasksList = (state = initialState, action) => {
+  switch (action.type) {
+    // case value:
+    // modifs
+    //break;
+    default:
+      return state;
+  }
+};
+
+const rootReducers = combineReducers({
+  tasksList,
+});
+
 // Store: attaché à notre appli React -> accessible depuis tous les composants
-export const store = createStore();
+export const store = createStore(rootReducers);

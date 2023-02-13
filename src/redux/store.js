95bbee07c95760fd26fb40918efed509ef32b55a
Antoine Goncalves <antoine.goncalves-isc.france@ibm.com>
2023-02-13 15:30:02 +0100

    installation de redux dans le projet + mise en place

---
+++ src/redux/store.js
@@ -0,0 +1,7 @@
+// Selectors: sélectionner une partie de notre store
+
+// Actions: fonction qui renvoient un objet aux reducers -> { type: MON_ACTION, payload: { DATA } }
+
+// Reducers: fonctions retournent un nouveau state
+
+// Store: attaché à notre appli React -> accessible depuis tous les composants

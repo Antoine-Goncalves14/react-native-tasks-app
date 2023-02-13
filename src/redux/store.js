6d615054bf9b3268ffd3420ddd314129672134bf
Antoine Goncalves <antoine.goncalves-isc.france@ibm.com>
2023-02-13 15:53:56 +0100

    création de l'action addTask

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,15 +4,39 @@ import {legacy_createStore as createStore, combineReducers} from 'redux';
 
 // Actions: fonction qui renvoient un objet aux reducers -> { type: MON_ACTION, payload: { DATA } }
 
+// ADD_Task
+const ADD_TASK = 'ADD_TASK';
+
+function addTask(title) {
+  return {
+    type: ADD_TASK,
+    payload: {
+      title,
+    },
+  };
+}
+
+// TOOGLE_TASK
+const TOOGLE_TASK = 'TOOGLE_TASK';
+
+// DELETE_TASK
+const DELETE_TASK = 'DELETE_TASK';
+
 // Reducers: fonctions retournent un nouveau state
 
 const initialState = [{id: 1, title: 'Init task', isCompleted: false}];
 
 const tasksList = (state = initialState, action) => {
   switch (action.type) {
-    // case value:
-    // modifs
-    //break;
+    case ADD_TASK:
+      return [
+        ...state,
+        {
+          id: new Date().getTime(),
+          title: action.payload.title,
+          isCompleted: false,
+        },
+      ];
     default:
       return state;
   }
